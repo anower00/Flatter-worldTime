44c9a8f7ba87388a1736721ca63e8bd4baa20ab5
DESKTOP-O65SORM\anowe <anower.hasan@gmail.com>
2020-09-09 19:29:52 +0600

    passing route data

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -13,10 +13,11 @@ class _LoadingState extends State<Loading> {
   void setUpWorldTime() async {
     WorldTime instance = WorldTime(location: 'Dhaka', flag: 'bd.png', url: 'Asia/Dhaka');
     await instance.getTime();
-    // print(instance.time);
-    setState(() {
-      time = instance.time;
-    });
+   Navigator.pushReplacementNamed(context, '/home', arguments: {
+     'location': instance.location,
+     'flag': instance.flag,
+     'time': instance.time,
+   });
   }
 
   @override
@@ -30,7 +31,7 @@ class _LoadingState extends State<Loading> {
     return Scaffold(
       body: Padding(
         padding: EdgeInsets.all(50.0),
-        child: Text(time),
+        child: Text('Loading'),
       ),
     );
   }
