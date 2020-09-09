289c472ffa20b646f658ca3f96ccd39a6bd962e4
DESKTOP-O65SORM\anowe <anower.hasan@gmail.com>
2020-09-09 16:15:02 +0600

    http package with Json placeholder

--- lib/pages/chose_location.dart
+++ lib/pages/chose_location.dart
@@ -7,25 +7,6 @@ class ChoseLocation extends StatefulWidget {
 
 class _ChoseLocationState extends State<ChoseLocation> {
 
-  void getData() async {
-    //simulate network request for username
-    String username = await Future.delayed(Duration(seconds: 3), () {
-      return 'Anower';
-    });
-    //simulate network request for address
-    String address = await Future.delayed(Duration(seconds: 2), () {
-      return 'Soulmaid, Dhaka';
-    });
-    print('$username - $address');
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    getData();
-    print('Hei its first load');
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
