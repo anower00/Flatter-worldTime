a1b726286dcde64eca35dc630b296f80dbb801c5
DESKTOP-O65SORM\anowe <anower.hasan@gmail.com>
2020-09-09 19:55:13 +0600

    dates formating with intl package and showing dates

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -1,5 +1,6 @@
 import 'package:http/http.dart';
 import 'dart:convert';
+import 'package:intl/intl.dart';
 
 class WorldTime {
   String location; //location name for the UI
@@ -29,7 +30,8 @@ class WorldTime {
       DateTime now = DateTime.parse(datetime);
       now = now.add(Duration(hours: int.parse(offset)));
       //set the time property
-      time = now.toString();
+      // time = now.toString();
+      time = DateFormat.jm().format(now);
     }catch(e){
       print('Something went wrong $e');
       time = 'Could not get time data';
